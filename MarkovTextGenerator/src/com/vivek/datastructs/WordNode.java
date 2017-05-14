b083eedaee80e381692dc5de11200c4358df6f46
vivek saxena <viveksaxena914@gmail.com>
2017-05-14 21:53:02 +0530

    Committing Markov Text Generator Project

---
+++ MarkovTextGenerator/src/com/vivek/datastructs/WordNode.java
@@ -0,0 +1,60 @@
+package com.vivek.datastructs;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class WordNode {
+	
+	private static final WordNode EMPTY_NODE = new WordNode("");
+
+	private final String word;
+	private final List<WordNode> probabilityList;
+
+	public WordNode(String word) {
+		super();
+		this.word = word;
+		this.probabilityList = new LinkedList<>();
+	}
+
+	public WordNode getNextWordNodes(Random random) {
+		if (this.probabilityList.size() == 0)
+			return EMPTY_NODE;
+
+		int index = random.nextInt(this.probabilityList.size());
+		return this.probabilityList.get(index);
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public List<WordNode> getProbabilityList() {
+		return probabilityList;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((word == null) ? 0 : word.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WordNode other = (WordNode) obj;
+		if (word == null) {
+			if (other.word != null)
+				return false;
+		} else if (!word.equals(other.word))
+			return false;
+		return true;
+	}
+}
---
+++ MarkovTextGenerator/src/com/vivek/model/MarkovModel.java
@@ -0,0 +1,75 @@
+package com.vivek.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import com.vivek.datastructs.WordNode;
+
+public class MarkovModel {
+
+	private final Random random;
+	private final Map<String, WordNode> model;
+	
+	public MarkovModel() {
+		this.model = new HashMap<>();
+		this.random = new Random();
+	}
+
+	public MarkovModel(Map<String, WordNode> model, Random random) {
+		super();
+		this.model = model;
+		this.random = random;
+	}
+
+	public String getNextWord(String word) {
+		WordNode wordNode = this.model.get(word);
+		if (wordNode != null) {
+			return wordNode.getNextWordNodes(random).getWord();
+		}
+		return "";
+	}
+
+	public void train(List<String> words) {
+		if (words == null || words.isEmpty()) {
+			return;
+		}
+
+		WordNode firstNode = null;
+		WordNode curNode = null;
+		for (String word : words) {
+			word = word.trim();
+			if (word.isEmpty()) {
+				continue;
+			}
+
+			if (model.isEmpty()) {
+				curNode = new WordNode(word);
+				this.model.put(word, curNode);
+				firstNode = curNode;
+			} else {
+				WordNode wordNode = this.model.get(word);
+				if (wordNode == null) {
+					wordNode = new WordNode(word);
+					this.model.put(word, wordNode);
+				}
+				curNode.getProbabilityList().add(wordNode);
+				curNode = wordNode;
+			}
+		}
+		curNode.getProbabilityList().add(firstNode);
+	}
+
+	public void displayModel() {
+		for (Map.Entry<String, WordNode> entry : this.model.entrySet()) {
+			System.out.print(entry.getKey() + " -> ");
+			WordNode node = entry.getValue();
+			for (WordNode w : node.getProbabilityList()) {
+				System.out.print(w.getWord() + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
